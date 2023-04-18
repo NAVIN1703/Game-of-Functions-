5eb80dfba0bbe7bbc96d38dee8f47f722c577c30
NAVIN1703 <129607328+NAVIN1703@users.noreply.github.com>
2023-04-18 10:10:50 +0530

    Add files via upload

---
+++ assignment-3(1).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total =total+num
+    return total
+my_list =  (8, 2, 3, 0, 7)
+result = sum_list(my_list)
+print(result)
+
+
+# In[ ]:
+
+
+
+
